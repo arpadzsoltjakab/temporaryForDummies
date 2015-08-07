c40edff975fcb5b1a899a6e86798c63f69751892
arpadzsoltjakab <arpadzsoltjakab@gmail.com>
2015-08-07 07:31:30 +0200

    refactoring reverse

--- temporaryForDummies/src/main/java/com/epam/me/temporaryForDummies/Solution.java
+++ temporaryForDummies/src/main/java/com/epam/me/temporaryForDummies/Solution.java
@@ -7,22 +7,20 @@ public class Solution {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-//		int t = in.nextInt();
 		Integer[] spam = new Integer[] { 1, 2, 3, 4, 5 };
 		ArrayList<Integer> list  = new ArrayList<>(Arrays.asList(spam));
-
-//		for(int i=0;i<t;i++){
-//			list.add(in.nextInt());
-//		}
 		System.out.println(reverse(list));
 	}
 
 	public static List<Integer> reverse (List<Integer> nums) {
-		List<Integer> reverseList = new ArrayList<Integer>(Arrays.asList(nums.remove(nums.size()-1)));
-		if(nums.size() > 0) {
-			reverseList.addAll(reverse(new ArrayList<Integer>(nums.subList(0, nums.size()))));
+		if(nums.size() > 1) {
+			Integer tmp = nums.get(nums.size()-1);
+			nums.set(nums.size()-1, nums.get(0));
+			nums.set(0, tmp);
+			List<Integer> subList = nums.subList(1, nums.size()-1);
+			reverse(subList);
 		}
-		return reverseList;
+		return nums;
 	}
 	
 	public static int getIndex(int[] input) {
