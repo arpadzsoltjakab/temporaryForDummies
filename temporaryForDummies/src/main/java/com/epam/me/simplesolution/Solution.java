0249b41a8b1293311e7fe2d5c92873e6d33adc12
Zsolt Jakab <zsolt_jakab@epam.com>
2015-08-24 10:36:39 +0200

    Sherlock and Squares

--- temporaryForDummies/src/main/java/com/epam/me/simplesolution/Solution.java
+++ temporaryForDummies/src/main/java/com/epam/me/simplesolution/Solution.java
@@ -3,15 +3,21 @@ package com.epam.me.simplesolution;
 import java.util.*;
 
 public class Solution {
-
-	public static void main(final String[] args) {
-		Integer i = 0;
-		sample(i);
-		System.out.println(i);
-    }
 	
-	public static void sample(Integer i) {
-		i.valueOf(100);
-		System.out.println(i);
+	private static Scanner in = new Scanner(System.in); 
+	
+	public static void main(String[] args) {
+    	int countOfTestCases = in.nextInt();
+		
+    	for(int i = 0; i < countOfTestCases; i++){			
+    		int beginOfRange	= in.nextInt();
+    		int endOfRange 		= in.nextInt();
+    		System.out.println(runTestCase(beginOfRange, endOfRange));
+		}
+    }
+
+	private static char[] runTestCase(int beginOfRange, int endOfRange) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }	
