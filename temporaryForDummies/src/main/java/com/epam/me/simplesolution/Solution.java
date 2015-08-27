2cba55b9a0a5d39b8222c2a80dd7e5f408766064
Zsolt Jakab <zsolt_jakab@epam.com>
2015-08-27 16:44:18 +0200

    no comment

--- temporaryForDummies/src/main/java/com/epam/me/simplesolution/Solution.java
+++ temporaryForDummies/src/main/java/com/epam/me/simplesolution/Solution.java
@@ -7,42 +7,46 @@ public class Solution {
 	private static Scanner in = new Scanner(System.in); 
 	
 	public static void main(String[] args) {
-    	int countOfTestCases = in.nextInt();
+    	int countOfTestCases = 1;
 		
     	for(int i = 0; i < countOfTestCases; i++){			  		
     		runTestCase();
 		}
     }
 	private static void runTestCase() {		
-		int sizeOfArray = in.nextInt();
-		long modulus = in.nextLong();
-		long[] numbers = new long[sizeOfArray];
-
-		for (int i = 0; i < numbers.length; i++) {
-			if(i == 0){
-				numbers[i] = in.nextLong() % modulus;
-			}
-			else{
-				numbers[i] = in.nextLong() + numbers[i-1] % modulus;	
-			}
-		}
-
-		TreeSet<Long> numbersInOrder = new TreeSet<Long>();
-		long maxSum = 0;
-		for (long value : numbers) {
-			if (numbersInOrder.isEmpty()) {
-				maxSum = value;
-				numbersInOrder.add(value);
+		int sizeOfListA = in.nextInt();
+		
+		HashMap<Integer, Integer> missingHash = new HashMap<Integer, Integer>();
+		
+		
+		Integer value;
+		for (int i = 0; i < sizeOfListA; i++) {
+			int key = in.nextInt();
+			if(missingHash.containsKey(key)) {
+				value = missingHash.get(key);
+				missingHash.put(key, ++value);
 			}
 			else {
-				maxSum = Math.max(value, maxSum);
-				Long nextValue = numbersInOrder.ceiling(value + 1);
-				if (nextValue != null) {
-					maxSum = Math.max(value - nextValue + modulus, maxSum);
-				}
-				numbersInOrder.add(value);
+				missingHash.put(key, 1);
+			}
+		}
+		
+		int sizeOfListB = in.nextInt();
+		Set<Integer> missingNumbers = new TreeSet<Integer>();
+		for (int i = 0; i < sizeOfListB; i++) {
+			int key = in.nextInt();
+			if((value = missingHash.get(key)) != null) {
+				missingHash.put(key, --value);
+			}
+			if (value == null || value == -1) {
+				missingNumbers.add(key);
 			}
 		}	
-		System.out.println(maxSum);
+		
+		Iterator iterator = missingNumbers.iterator();
+
+		while (iterator.hasNext()) {
+			System.out.print(iterator.next() + " ");
+		}
 	}
 }	
